51e700c81c0c759f1809dc27e9d8d6b7988e21b5
HuzaiKing <huzaifayaseen1172@gmail.com>
2023-06-02 00:23:38 +0500

    Problem and its solution related Alphabetic Patterns

--- asterisk.cpp
+++ asterisk.cpp
@@ -46,7 +46,7 @@ int main()
     //     cout<<endl;
     //     i++;
     //    }
-    cout << endl;
+    // cout << endl;
     /*
         1 2 3 4
         5 6 7 8
@@ -135,37 +135,196 @@ int main()
     4 5 6 7
     */
 
-//    int i = 1;
-//     while (i <= no)
-//     {
-//         int j = 1;
-//         while (j <= i)
-//         {
-//             cout << i+j -1<<" ";
-//             j++;
-//         }
-//         cout << endl;
-//         i++;
-//     }
+    //    int i = 1;
+    //     while (i <= no)
+    //     {
+    //         int j = 1;
+    //         while (j <= i)
+    //         {
+    //             cout << i+j -1<<" ";
+    //             j++;
+    //         }
+    //         cout << endl;
+    //         i++;
+    //     }
 
     /*
     1
     2 1
     3 2 1
-    4 3 2 1              
+    4 3 2 1
     */
 
-   int i=1;
-   while(i<=no)
-   {
-    int j=1;
-    while(j<=i)
-    {
-        cout<<i-j+1<<" ";
-        j++;
-    }
-    cout<<endl;
-    i++;
-   }
-    
+    //    int i=1;
+    //    while(i<=no)
+    //    {
+    //     int j=1;
+    //     while(j<=i)
+    //     {
+    //         cout<<i-j+1<<" ";
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    //    }
+
+    /*
+    A A A
+    B B B
+    C C C
+    */
+
+    //   int i=1;
+    //    while(i<=no)
+    //    {
+    //     int j=1;
+    //     while(j<=no)
+    //     {
+    //         cout<<char(64+i);
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    //    }
+
+    /*
+    A B C
+    A B C
+    A B C
+    */
+
+    //    int i=1;
+    //    while(i<=no)
+    //    {
+    //     int j=1;
+    //     while(j<=no)
+    //     {
+    //         cout<<char(64+j);
+    //         j++;
+    //     }
+    //     cout<<endl;
+    //     i++;
+    //    }
+
+    /*
+        A B C
+        D E F
+        G H I
+    */
+
+    // int i = 1;char c='A';
+    // while (i <= no)
+    // {
+    //     int j = 1;
+    //     while (j <= no)
+    //     {
+    //         cout<<c<<" ";
+    //         c++;
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    /*
+        A B C
+        B C D
+        C D E
+    */
+
+    // int i = 1;
+    // while (i <= no)
+    // {
+    //     int j = 1;
+    //     while (j <= no)
+    //     {
+    //         cout << char(64 + i + j - 1) << " ";
+    //         j++;
+    //     }
+    //     cout << endl;
+    //     i++;
+    // }
+
+    /*
+    A
+    B B
+    C C C
+    */
+
+//    int i=1;
+//    while(i<=no)
+//    {
+//     int j=1;
+//     while(j<=i)
+//     {
+//         cout<<char(64+i)<<" ";
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+//    }
+
+    /*
+    A
+    B C
+    D E F
+    G H I J
+    */
+
+//    int i=1;
+//    char c='A';
+//    while(i<=no)
+//    {
+//     int j=1;
+//     while(j<=i)
+//     {
+//         cout<<c<<" ";
+//         c++;
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+//    }
+
+    /*
+    A
+    B C
+    C D E
+    D E F G
+    */
+
+//    int i=1;
+//    while(i<=no)
+//    {
+//     int j=1;
+//     while(j<=i)
+//     {
+//         cout<<char(64+i+j-1)<<" ";
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+//    }
+
+
+    /*
+    D
+    C D
+    B C D
+    A B C D
+    */
+//    int i=1;
+//    while(i<=no)
+//    {
+//     int j=1;
+//     while(j<=i)
+//     {
+//         cout<<char(64+no-i+j)<<" ";
+//         j++;
+//     }
+//     cout<<endl;
+//     i++;
+//    }
+
+
 }
