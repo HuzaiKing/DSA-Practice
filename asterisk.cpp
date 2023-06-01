490ea2b6694be60e17994bcc1d6f1eecfc62a1e6
HuzaiKing <huzaifayaseen1172@gmail.com>
2023-06-02 00:54:22 +0500

    Accute Triangle examples

--- asterisk.cpp
+++ asterisk.cpp
@@ -484,4 +484,37 @@ int main()
     //     cout << endl;
     //     i++;
     // }
+
+    /*
+          1
+        1 2 1
+      1 2 3 2 1
+    1 2 3 4 3 2 1
+
+    */
+
+   int i=1;
+   while(i<=no)
+   {
+    int j=1;
+    while(j<=no-i)
+    {
+        cout<<" ";
+        j++;
+    }
+    j=1;
+    while(j<=i)
+    {
+        cout<<j;
+        j++;
+    }
+    j=i-1;
+    while(j>=1)
+    {
+        cout<<j;
+        j--;
+    }
+    cout<<endl;
+    i++;
+   }
 }
